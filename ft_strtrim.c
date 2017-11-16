0c5cdca01de610d4e8ab2ff26db2e6a1ed4a8cd6
Kelian DUMARAIS <kdumarai@tudent.42.fr>
2017-11-16 11:33:54 +0100

    Updated strtrim to work perfectly while being nice to the eye (at leat mine :p)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 18:04:20 by kdumarai          #+#    #+#             */
-/*   Updated: 2017/11/15 14:59:48 by kdumarai         ###   ########.fr       */
+/*   Updated: 2017/11/16 11:26:08 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ static int				is_whitechar(char c)
 
 static unsigned int		first_last_index(int first, const char *str)
 {
-	int		i;
+	unsigned int	i;
 
 	i = (first) ? 0 : ft_strlen((char*)str) - 1;
-	while (is_whitechar(str[i]) && i >= 0)
+	while (is_whitechar(str[i]) && (i > 0 || first))
 		i += (first) ? 1 : -1;
 	return (i);
 }
@@ -37,7 +37,7 @@ char					*ft_strtrim(char const *s)
 		return (NULL);
 	findex = first_last_index(1, s);
 	lindex = first_last_index(0, s);
-	if (findex >= lindex)
+	if (lindex == 0)
 		return (ft_strnew(0));
 	if (!(ret = ft_strsub(s, findex, (size_t)(lindex - findex + 1))))
 		return (NULL);
---
+++ misc/tests/ft_strlcat/strlcat_test.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strstr_test.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 13:52:51 by kdumarai          #+#    #+#             */
+/*   Updated: 2017/11/08 18:35:21 by kdumarai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+int		main(int ac, char **av)
+{
+	char	destm[20] = "some text";
+	char	desto[20] = "some text";
+
+	if (ac < 3)
+		return (1);
+#ifdef __MY__
+	printf("Mine: %lu\n", ft_strlcat(destm, av[1], atoi(av[2])));
+	printf("\tdest = %s\n", destm);
+#else
+	(void)destm;
+#endif
+	printf("Orig: %lu\n", strlcat(desto, av[1], atoi(av[2])));
+	printf("\tdest = %s\n", desto);
+	return (0);
+}
