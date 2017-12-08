d011296440566ce8df3ac0f0b2e504b0bd0942bd
Kelian DUMARAIS <kdumarai@tudent.42.fr>
2017-12-08 17:53:58 +0100

    Fixed libft.h

--- misc/checks/check_p2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sdir=`dirname "$0"`
-
-list="itoa memalloc memdel putchar putchar_fd putendl putendl_fd putnbr putnbr_fd putstr putstr_fd strclr strdel strequ striter striteri strjoin strmap strmapi strnequ strnew strsplit strsub strtrim"
-src_path="$sdir"/../srcs/supp
-
-sh "$sdir"/check_parts.sh "$src_path" "$list"
