ab9af10113ff6a530ff081655298fbeff79b0a0b
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-05-09 19:47:45 +0200

    Fixed dumb typo

--- srcs/mem/ft_memccpy.c
+++ srcs/mem/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 15:57:00 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/05/09 19:44:36 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/05/09 19:47:25 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 		dstc[i] = srcc[i];
 		if (srcc[i] == (unsigned char)c)
 			return ((void*)&dstc[i + 1]);
-		}
 		i++;
 	}
 	return (NULL);
