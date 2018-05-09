5b160e0288b1f55fecf2763458f2ffc3fc96b5e9
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-05-10 00:51:16 +0200

    update to ft_putubt

--- srcs/print/ft_putubt.c
+++ srcs/print/ft_putubt.c
@@ -5,28 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/05 16:28:21 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/05/05 16:32:54 by kdumarai         ###   ########.fr       */
+/*   Created: 2018/05/09 22:45:10 by kdumarai          #+#    #+#             */
+/*   Updated: 2018/05/09 22:46:11 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putubt(t_btree *bt, void (*ft_putelem)(void*, size_t))
+static void	ft_putubt_core(t_btree *bt,
+							void (*ft_putelem)(void*, size_t),
+							int lvl)
 {
-	if (!bt)
-		return ;
+	ft_putnbr(lvl);
+	ft_putstr(": ");
 	ft_putelem(bt->data, bt->data_size);
 	if (bt->left)
 	{
+		ft_putnbr(lvl);
+		ft_putstr(": ");
 		ft_putendl("turn left");
-		ft_putubt(bt->left, ft_putelem);
+		ft_putubt_core(bt->left, ft_putelem, lvl + 1);
+		ft_putnbr(lvl + 1);
+		ft_putstr(": ");
 		ft_putendl("back up");
 	}
 	if (bt->right)
 	{
+		ft_putnbr(lvl);
+		ft_putstr(": ");
 		ft_putendl("turn right");
-		ft_putubt(bt->right, ft_putelem);
+		ft_putubt_core(bt->left, ft_putelem, lvl + 1);
+		ft_putnbr(lvl + 1);
+		ft_putstr(": ");
 		ft_putendl("back up");
 	}
+}
+
+void		ft_putubt(t_btree *bt, void (*ft_putelem)(void*, size_t))
+{
+	if (!bt || !ft_putelem)
+		return ;
+	ft_putubt_core(bt, ft_putelem, 0);
 }
