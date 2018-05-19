a3e7bc67b4a1d39256bf2108eeba33006a4bfef7
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-05-19 19:37:08 +0200

    if *str == NULL, then *str is strdup(add)

--- srcs/string/ft_stradd.c
+++ srcs/string/ft_stradd.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 20:42:58 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/02/21 22:52:52 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/05/19 19:36:25 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int		ft_stradd(char **str, char *add)
 
 	if (!str || !add)
 		return (FALSE);
+	if (!*str)
+	{
+		*str = ft_strdup(add);
+		return (TRUE);
+	}
 	tmp = *str;
 	if (!(*str = ft_strjoin(*str, add)))
 	{
