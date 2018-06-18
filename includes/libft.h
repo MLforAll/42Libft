5d432eae206ee55565fb66a8fe633b34dd037e73
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-06-18 23:44:38 +0200

    added stdint.h header for uintptr_t and other [u]intTYPE_t typedefs

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 10:58:20 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/06/14 04:26:59 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/06/18 23:44:05 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <string.h>
+# include <stdint.h>
 
 # include "mem.h"
 # include "str.h"
