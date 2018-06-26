db808784d7e4183ab05199961a220669d7c56eec
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-06-27 00:01:40 +0200

    potential issue with ft_ttabcat

--- srcs/t_tab/ft_ttabcat.c
+++ srcs/t_tab/ft_ttabcat.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/19 01:59:20 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/06/14 03:32:58 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/06/27 00:00:48 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void	ft_ttabcat(t_tab *mtab, void *ntab, size_t size)
 {
 	void	*newdat;
+	void	*dest;
 
 	if (mtab->oc_size + size > mtab->bufflen)
 	{
@@ -25,6 +26,7 @@ void	ft_ttabcat(t_tab *mtab, void *ntab, size_t size)
 		free(mtab->data);
 		mtab->data = newdat;
 	}
-	ft_memcpy((void*)((uintptr_t)mtab->data + mtab->oc_size), ntab, size);
+	dest = (void*)((uintptr_t)mtab->data + mtab->oc_size);
+	ft_memcpy(dest, ntab, size);
 	mtab->oc_size += size;
 }
