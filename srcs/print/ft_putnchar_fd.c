1995f8db9d5611cd14e4e41cf25105e49b557312
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-07-15 21:32:35 +0200

    fix mem leak

--- srcs/print/ft_putnchar_fd.c
+++ srcs/print/ft_putnchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 16:37:48 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/05/25 16:44:31 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/07/15 21:32:26 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,5 @@ void	ft_putnchar_fd(char c, size_t n, int fd)
 	ft_memset(str, c, n);
 	str[n] = '\0';
 	ft_putstr_fd(str, fd);
+	free(str);
 }
