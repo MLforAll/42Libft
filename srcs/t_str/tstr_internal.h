2a7fbc2577c52c83e77980d71b7895ab38fb71c7
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-08-08 17:05:30 +0200

    missing header for linux

--- srcs/t_str/tstr_internal.h
+++ srcs/t_str/tstr_internal.h
@@ -6,13 +6,14 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 05:13:41 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/08/01 20:13:41 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/08/08 17:04:15 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TSTR_INTERNAL_H
 # define TSTR_INTERNAL_H
 
+# include <unistd.h>
 # include "libft.h"
 
 int		tstrncat_core(t_str *vstr, const char *add, ssize_t len);
