421a4207bd0743cfab33ffd22d5d3cb84fe451e3
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-08-09 22:08:29 +0200

    fix when addlen == 0

--- srcs/t_str/tstrcat_core.c
+++ srcs/t_str/tstrcat_core.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/08 00:52:02 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/08/01 20:14:04 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/08/09 22:06:28 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		tstrncat_core(t_str *vstr, const char *add, ssize_t len)
 	if (!add)
 		return (FALSE);
 	addlen = (len == -1) ? ft_strlen(add) : (size_t)len;
-	if (addlen == 0 || !ft_tstr_cpycore(vstr, addlen))
+	if (!ft_tstr_cpycore(vstr, addlen))
 		return (FALSE);
 	if (len == -1)
 		(void)ft_strcpy(vstr->s + vstr->len, add);
