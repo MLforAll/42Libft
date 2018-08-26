9081157efb61f64cc439c4720fa00c2cb210eb78
Kelian DUMARAIS (MLforAll) <kdumarai@student.42.fr>
2018-08-27 00:09:55 +0200

    wtf was this code?

--- srcs/print/ft_putnbrl_fd.c
+++ srcs/print/ft_putnbrl_fd.c
@@ -14,18 +14,6 @@
 
 void	ft_putnbrl_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putendl_fd("-2147483648", fd);
-		return ;
-	}
-	if (n < 0)
-	{
-		n = -n;
-		ft_putchar_fd('-', fd);
-	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + '0', fd);
+	ft_putnbr_fd(n, fd);
 	ft_putchar_fd('\n', fd);
 }
