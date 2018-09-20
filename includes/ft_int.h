623960b01abca929ac0f1061e6f4cc9aba7fa7a7
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-09-20 03:58:17 +0200

    t_[u]int64 OK on both LP64 and LLP64

--- includes/ft_int.h
+++ includes/ft_int.h
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/28 23:09:03 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/08/01 23:50:08 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/09/20 03:57:23 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,43 @@
 ** 8-bit ones
 */
 
-typedef unsigned char	t_uint8;
-typedef char			t_int8;
+typedef unsigned char		t_uint8;
+typedef char				t_int8;
 
 /*
 ** 32-bit ones
 */
 
-typedef unsigned int	t_uint32;
-typedef int				t_int32;
+typedef unsigned int		t_uint32;
+typedef int					t_int32;
 
 /*
 ** 64-bit ones
 */
 
-typedef unsigned long	t_uint64;
-typedef long			t_int64;
+#ifdef _WIN32
+
+typedef unsigned long long	t_uint64;
+typedef long long			t_int64;
+
+#else
+
+typedef unsigned long		t_uint64;
+typedef long				t_int64;
+
+#endif
 
 /*
 ** ptr
 */
 
-typedef t_uint64		t_uintptr;
-typedef ptrdiff_t		t_intptr;
+typedef t_uint64			t_uintptr;
+typedef ptrdiff_t			t_intptr;
 
 /*
 ** characters
 */
 
-typedef int				t_wchar;
+typedef int					t_wchar;
 
 #endif
